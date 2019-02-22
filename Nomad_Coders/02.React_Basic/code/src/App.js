b3a1f99c9be7999c7848e58cb1eef9ccb1bebda9
Dudu <engus983@gmail.com>
2019-02-22 19:59:28 +0900

    feat : ADD React JS Basic #6
    React JS Basic #6-2 Completed!

--- Nomad_Coders/02.React_Basic/code/src/App.js
+++ Nomad_Coders/02.React_Basic/code/src/App.js
@@ -8,37 +8,13 @@ class App extends Component {
 
   // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> component
 
-  state = {
-    
-  }
+  state = {}
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title: "Iron-Man",
-            poster: "https://cnet1.cbsistatic.com/img/keKJmjkTp20tFz62rrzBQyRrPc0=/570x0/2013/10/10/1adcb0d0-4de3-11e3-945a-14feb5ca9861/iron_man_3.jpg"
-          },
-          {
-            title: "Captain-America",
-            poster: "https://cdn3.movieweb.com/i/article/lLaIH7FR2SocKiB2YNLM1h9zVumGPM/798:50/Avengers-Infinity-War-Captain-America-Phone-Number-Easter.jpg",
-          },
-          {
-            title: "Thor",
-            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp2lAISIp1nFL0W07MVsr0dA3Fl3PzSNozZne622x6vSXEoRkzpA"
-          },
-          {
-            title: "Spider-Man",
-            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsifsHqRMnIqNDoUORl2QfYbp9zJNsadimTAlPpLa7B7v-HoW3"
-          },
-          {
-            title : "Docktor Strange",
-            poster : "http://thumbnail.egloos.net/600x0/http://pds25.egloos.com/pds/201610/26/51/e0098851_58108536ba52e.png"
-          }
-        ]
-      })
-    }, 3000)
+    fetch('https://yts.am/api/v2/list_movies.json?quality=3D?sort_by=rating')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
