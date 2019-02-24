be7ad84d35de9ec03071e28709011a2ac5a671a9
Duhyeon_Home <engus983@gmail.com>
2019-02-25 08:03:05 +0900

    feat : ADD ReactJS Basic #7
    ReactJS Basic #7-2 CSS 추가

--- Nomad_Coders/02.React_Basic/code/src/Movie.js
+++ Nomad_Coders/02.React_Basic/code/src/Movie.js
@@ -1,24 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis'
 import './Movie.css';
 
-function Movie({ title, poster, genres, synopsis }) {
+function Movie({title, poster, genres, synopsis}){
     return (
         <div className="Movie">
-            <div className="Movie__Columns">
+            <div className="Movie__Column">
                 <MoviePoster poster={poster} alt={title} />
-                <h1>{title}</h1>
-            </div>
-            <div className="Movie__Columns">
+            </div>   
+            <div className="Movie__Column">
                 <h1>{title}</h1>
                 <div className="Movie__Genres">
                     {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
                 </div>
-                <p className="Movie__Synopsis">
-                    {synopsis}
-                </p>
+                <div className="Movie__Synopsis">
+                <LinesEllipsis
+                    text={synopsis}
+                    maxLine='3'
+                    ellipsis='...'
+                    trimRight
+                    basedOn='letters'
+                    />   
+                </div>
             </div>
         </div>
+    )
+}
+
+function MoviePoster({poster, alt}){
+    return (
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
+    )
+}
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genre">{genre}</span>
     )
 }
 
@@ -26,27 +44,15 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
     genres: PropTypes.array.isRequired,
-    synopsis: PropTypes.string.isRequired,
-}
-
-function MoviePoster({ poster, alt }) {
-    return (
-        <img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
-    )
+    synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
     poster: PropTypes.string.isRequired,
-    alt : PropTypes.string.isRequired
+    alt: PropTypes.string.isRequired
 }
 
-function MovieGenre({ genre }) {
-    return (
-        <span className="Movie__Genre">{genre}</span>
-    )
-}
-
-MovieGenre.prototype = {
+MovieGenre.propTypes ={
     genre: PropTypes.string.isRequired
 }
 
