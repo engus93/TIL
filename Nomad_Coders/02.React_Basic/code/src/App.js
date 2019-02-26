e887ef599195e93c81e843f03a8de4de49ee7267
Dudu <engus983@gmail.com>
2019-02-26 22:00:30 +0900

    feat : ADD RestAPI
    Rest API 자료 정리

--- Nomad_Coders/02.React_Basic/code/src/App.js
+++ Nomad_Coders/02.React_Basic/code/src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
   }
 
   _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?quality=3D?sort_by=download_count')
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
       .then(response => response.json())
       .then(json => json.data.movies)
       .catch(err => console.log(err))
